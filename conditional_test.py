dd3025eda266e373314a9906f82ae1f31ca1d931
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-06-28 09:58:51 -0400

    Update conditional_test.py

--- conditional_test.py
+++ conditional_test.py
@@ -105,3 +105,24 @@ if age >=18:
 else:
      print("Sorry you are too young to vote.")
      print("Please register to vote as soon as you turn 18!")
+
+
+#amusement_park.py
+age=12
+if age < 4:
+     print("\nYour admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+     print("Your admission cost is $10.")
+#shorter version     
+
+age=21
+if age <4:
+     price=0
+elif age <18:
+     price=5
+else:
+     price=10
+print("Your admission cost is $"+str(price)+".")
+
