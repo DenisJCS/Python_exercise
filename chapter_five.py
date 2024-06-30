2f4e0806e3be2146c6ca4a8a60b29a1a354b6964
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-06-30 14:45:42 -0400

    Update chapter_five.py

--- chapter_five.py
+++ chapter_five.py
@@ -32,3 +32,26 @@ for requested_topping in requested_toppings:
         print(f"Sorry we don't have {requested_topping}")
 print("\nFinished making your pizza!")
 
+#Exercise 5-8 page 103
+names = ['Jake', 'admin', 'Alex', 'Dave', 'Paul', 'Charlie']
+for name in names:
+    if name == 'admin':
+        print(f"Hello {name} would you like to see status reports?")
+    else:
+        print(f"Hello {name} thank you for logging in again! ")
+#Exercise 5-8 Checking if list is empty
+names = []
+if names:
+    for name in names:
+        print(f"Hello user {name}")
+else:
+    print("We need to ind some users!")
+
+# 5-10 Ckecking that user names are uniq
+current_users = ['Madiso', 'Mendal', 'Charlie' ,'Bravo', 'Alfa']
+new_users = ['Bravo', 'Elison', 'Charlie', 'Bane', 'Scot']
+for user in new_users:
+    if user in current_users:
+        print(f"Name {user}is not available please choose other name")
+    else:
+     print(f"Name available , welcome {user}")
