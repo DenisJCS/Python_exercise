92388300f8c141ce1d847de0e02fb00663fcdf0f
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-11 16:13:24 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -9,3 +9,16 @@ def max_num(nums):
 #Uncomment the line below when your function is done
 #print(max_num([50, -10, 0, 75, 20]))
 print(max_num([50, -10, 0, 75, 20]))
+
+#Write your function here
+def same_values(lst1, lst2):
+  """Returns list where values equal"""
+  new_list = []
+  for index in range(len(lst1)):
+        if lst1[index] == lst2[index]:
+            new_list.append(index)
+  return new_list
+
+#Uncomment the line below when your function is done
+#print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
