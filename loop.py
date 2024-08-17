2979bea4d2ca9dacd7d999c5593f2cee730e1e49
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-17 10:43:08 -0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -133,3 +133,18 @@ def first_three_multiples(num):
 # should print 0, 0, 0, and return 0
 first_three_multiples(10)
 first_three_multiples(0)
+
+
+
+# Write your dog_years function here:
+def dog_years(name, age):
+  return name + ", you are " +str(age*7) + " years old in dog year"
+  
+
+# Uncomment these function calls to test your dog_years function:
+#print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+#print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+print(dog_years("Lola", 16))
+print(dog_years("Baby", 0))
