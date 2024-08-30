69904dc63460184b507e5aafa12f9e35833f22f7
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-29 22:16:34 -0400

    Update magic_ball.py

--- magic_ball.py
+++ magic_ball.py
@@ -1,1 +1,16 @@
-
+import random
+name = ""
+question = input()
+answers = [
+    "Yes ",
+    "No",
+    "Try again",
+    "100 %",
+    "Better not tell you now",
+    "My sources say no",
+    "Outlook not so good",
+    " Very doubtful",
+    " If it is alsmost end , you will", 
+    "Try harder",
+]
+answers = random.choice(answers)
