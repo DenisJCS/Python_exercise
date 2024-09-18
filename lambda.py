88543661b34eb2512fbf576913b23647b7abfa5d
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-17 22:07:48 -0400

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -6,3 +6,11 @@ numbers = [1, 2, 3, 4, 5]
 squared = list(map(lambda x: x ** 2, numbers)) 
 
 print(squared)  # Output: [1, 4, 9, 16, 25] 
+
+#The filter() function creates a new list of elements for which the given lambda function returns True:
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers)) 
+
+print(even_numbers)  # Output: [2, 4, 6, 8, 10] 
