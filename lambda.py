a4794c618beaafa4f5c5f87977ba08336895693e
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-19 22:44:30 -0400

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -14,3 +14,20 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers)) 
 
 print(even_numbers)  # Output: [2, 4, 6, 8, 10] 
+
+
+#Lambda function 
+
+def double(x): 
+
+    return x * 2 
+
+  
+
+numbers = [1, 2, 3, 4, 5] 
+
+doubled_numbers = map(double, numbers) 
+
+  
+
+print(list(doubled_numbers))  # Output: [2, 4, 6, 8, 10] 
