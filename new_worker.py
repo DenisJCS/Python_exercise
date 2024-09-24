37280a2f18f26843b858a4446a2570a7777dd6af
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-23 22:29:44 -0400

    Update new_worker.py

--- new_worker.py
+++ new_worker.py
@@ -20,3 +20,20 @@ print(new_account)
 
 
 
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+  """Generates temporary password"""
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
+
+print(temp_password)
+
+
+
+
+
+
