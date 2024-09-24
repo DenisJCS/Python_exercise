bd40fcced9ab9ff2c8bd26990050281013ddaeae
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-24 00:01:23 -0400

    Update new_worker.py

--- new_worker.py
+++ new_worker.py
@@ -50,3 +50,25 @@ final_word = company_motto[-4:]
 print(final_word)
 
 
+# Final test
+def username_generator(first_name, last_name):
+  if len(first_name) < 3:
+    user_name = first_name
+  else:
+    user_name = first_name[0:3]
+  if len(last_name) < 4:
+    user_name += last_name
+  else:
+    user_name += last_name[0:4]
+  return user_name
+
+
+def password_generator(user_name):
+  """Generates temp. password by user name"""
+  password = ""
+  for i in range(0, len(user_name)):
+    password += user_name[i-1]
+  return password
+
+
+
