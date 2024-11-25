c8aa6fd57a1225584abef17e4c24d71b39e8b991
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-11-24 21:31:11 -0500

    Create formatting.py

---
+++ formatting.py
@@ -0,0 +1,14 @@
+poem_title = "spring storm"
+poem_author = "William Carlos Williams"
+
+poem_title_fixed = poem_title.title()
+print (poem_title_fixed)
+
+print(poem_title)
+print(poem_author)
+
+poem_author_fixed = poem_author.upper()
+print(poem_author_fixed)
+
+print(poem_author)
+print(poem_author_fixed)
