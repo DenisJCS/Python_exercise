68993f8d90e04e850a85212e869f39d39eb6fe67
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-12-04 23:03:14 -0500

    Update magic_ball.py

--- magic_ball.py
+++ magic_ball.py
@@ -1,1 +1,28 @@
+import random
+name = ""
+question = input()
+answers = [
+    "Yes ",
+    "No",
+    "Try again",
+    "100 %",
+    "Better not tell you now",
+    "My sources say no",
+    "Outlook not so good",
+    " Very doubtful",
+    " If it is alsmost end , you will", 
+    "Try harder",
+]
+answers = random.choice(answers)
+while question == "":
+    question = input("Please enter a question (or type 'q' to quit): ")
+    if question.lower() == "q":
+        print("Goodbye!")
+        break
+if question.lower() != "q":
+    if name == "":
+        print("Question"+ question)
+    else:
+        print(name + " asks: " + question)
+print("Magic 8-Ball's answer: " + answers)
 
