e302fe5bb9652fc438f8273db6da38ba7da40448
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-03 23:17:48 -0500

    Update piffagore.py

--- piffagore.py
+++ piffagore.py
@@ -8,3 +8,29 @@ d= math.sqrt(l**2 + w**2)
 
 print("Daioganal id "d)
 
+#Some calculations :)
+>>> x=1
+>>> x +=5
+>>> x
+6
+>>> y=100
+>>> y -=50
+>>> y
+50
+>>> w=10
+>>> w !=5
+True
+>>> w < 50
+True
+>>> w=100
+>>> 
+>>> w
+100
+>>> x=24
+>>> x=5
+>>> x
+5
+>>> x=5
+>>> x !=5
+False
+>>> 
