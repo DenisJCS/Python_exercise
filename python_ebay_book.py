5d887339396c42ab2dda703afe29c021311dadf2
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-03 23:29:03 -0500

    Create python_ebay_book.py

---
+++ python_ebay_book.py
@@ -0,0 +1,8 @@
+>>> home = "Nort Port"
+>>> if home == "Nort Port":
+...     print("Hello Denis from Nort Port :)")
+... else:
+...     print("No no you are from NORTH PORT :)")
+... 
+Hello Denis from Nort Port :)
+>>> 
