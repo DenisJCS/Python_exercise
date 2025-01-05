7df6821d8295924e8fe72b3239a9a3c022c10e0f
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-04 23:38:21 -0500

    Update division.py

--- division.py
+++ division.py
@@ -1,1 +1,21 @@
+while True:
+    user_input1 = input("Enter any number (or 'q' to stop):")
+    
+    if user_input1.lower() == 'q':
+        break
+    num1 = float(user_input1)
+
+    user_input2 = input("Enter any number (or 'q' to stop):")
+    if user_input2.lower() == 'q':
+        break
+    num2 = float(user_input2)
+    if num2 == 0:
+        print("Division by 0 not allowed")
+    else:
+        print(num1/num2) 
+
+
+
+
+
 
